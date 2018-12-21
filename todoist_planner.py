0128f02ac8ddaa36c30bf638a443125b797aac4d
Louis Martin <louismartin@fb.com>
2018-12-21 19:37:59 +0100

    Refactor attributes (namedtuples), delete and complete tasks

--- todoist_planner.py
+++ todoist_planner.py
@@ -1,3 +1,4 @@
+from collections import namedtuple, OrderedDict
 import math
 from pathlib import Path
 import re
@@ -9,11 +10,18 @@ from todoist.api import TodoistAPI
 __file__ = '/Users/louismartin/dev/todoist-planner/Untitled.ipynb'
 REPO_DIR = Path(__file__).resolve().parent
 token_filepath = REPO_DIR / 'token'
+Attribute = namedtuple('Attribute', ['parse_regex', 'str_format', 'ask_text'])
+attributes = OrderedDict({
+        'importance': Attribute(r'<i(\d)>', '<i{}>', 'How important is this task? (1-4): '),
+        'urgency': Attribute(r'<u(\d)>', '<u{}>', 'How urgent is this task? (1-4): '),
+        'duration': Attribute(r'<(\d+)m>', '<{}m>', 'How long will this task take? (minutes): '),
+})
 
 
 def ask_for_token():
     text = 'Please copy your todoist API token.'
     text += '\nYou can find it in "Todoist Settings -> Integrations -> API token":'
+    text += '\nhttps://en.todoist.com/prefs/integrations\n'
     token = input(text)
     with token_filepath.open('w') as f:
         f.write(token + '\n')
@@ -23,7 +31,10 @@ def read_token():
     if not token_filepath.exists():
         ask_for_token()
     with token_filepath.open('r') as f:
-        return f.read().rstrip('\n')
+        token = f.read().rstrip('\n')
+    if token == '':
+        ask_for_token()
+    return token
 
 
 def get_project_id_by_name(name, api):
@@ -47,22 +58,8 @@ def get_labels(api):
     return {label['name']: label['id'] for label in api.labels.all()}
 
 
-def get_importance(task):
-    match = re.search(r'<i(\d)>', task['content'])
-    if match is None:
-        return None
-    return int(match.groups()[0])
-
-
-def get_urgency(task):
-    match = re.search(r'<u(\d)>', task['content'])
-    if match is None:
-        return None
-    return int(match.groups()[0])
-
-
-def get_duration(task):
-    match = re.search(r'<(\d+)m>', task['content'])
+def parse_attribute(task, parse_regex):
+    match = re.search(parse_regex, task['content'])
     if match is None:
         return None
     return int(match.groups()[0])
@@ -93,12 +90,11 @@ def get_priority(task):
 
 
 def is_labeled(task):
-    importance = get_importance(task)
-    urgency = get_urgency(task)
-    duration = get_duration(task)
-    if importance is None or urgency is None or duration is None:
+    attribute_values = {attr_name: parse_attribute(task, attribute.parse_regex)
+                        for attr_name, attribute in attributes.items()}
+    if None in attribute_values.values():
         return False
-    priority = compute_priority(importance, urgency)
+    priority = compute_priority(attribute_values['importance'], attribute_values['urgency'])
     actual_priority = get_priority(task)
     if priority != actual_priority:
         print(f'Warning: actual_priority != computed_priority for task "{task["content"]}"')
@@ -109,20 +105,41 @@ def is_labeled(task):
 def label_task(task):
     stripped_content = re.sub(r'<.+>', '', task['content']).strip()
     print(f'"{stripped_content}"')
-    importance = int(input('How important is this task? (1-4): '))
-    # TODO: Better way to handle this case
-    if importance == 0:
-        return
-    urgency = int(input('How urgent is this task? (1-4): '))
-    duration = int(input('How long will it take? (minutes): '))
-    priority = compute_priority(importance, urgency)
+    attribute_values = {}
+    for attr_name, attribute in attributes.items():
+        current_value = parse_attribute(task, attribute.parse_regex)
+        ask_text = attribute.ask_text
+        if current_value is not None:
+            # TODO: Remove this value if user inputs something
+            ask_text += str(current_value)
+        value = input(ask_text)
+        # TODO: Better way to handle this case
+        if value == '':   # User just validated current value
+            value = current_value
+        elif value == 'n':  # next
+            return
+        elif value == 'd':  # delete
+            task.delete()
+            return
+        elif value == 'e':  # edit
+            task.update(content=input('New task content: \n'))
+            label_task(task)
+            # TODO: Maybe we should keep the tasks current attribute values
+            # attributes)
+            return
+        elif value == 'c':  # complete
+            task.complete()
+            return
+        attribute_values[attr_name] = int(value)
+    priority = compute_priority(attribute_values['importance'], attribute_values['urgency'])
     # TODO: Store this information in a note?
-    task.update(content=f'{stripped_content} <i{importance}> <u{urgency}> <{duration}m>',
+    attributes_str = ' '.join([attributes[attr_name].str_format.format(value)
+                               for attr_name, value in attribute_values.items()])
+    task.update(content=f'{stripped_content} {attributes_str}',
                 priority=(4 - priority) + 1)
-    api.commit()
 
 
-def label_tasks(tasks):
+def label_tasks(tasks, api):
     unlabeled_tasks = [task for task in tasks if not is_labeled(task)]
     if not unlabeled_tasks:
         print('No unlabeled tasks.')
@@ -132,12 +149,26 @@ def label_tasks(tasks):
     for i, task in enumerate(unlabeled_tasks):
         sys.stdout.write(f'{i+1}.')
         label_task(task)
+        api.commit()
         print('\n')
     print('~' * 50)
 
 
 def sort_tasks(tasks):
-    return sorted(tasks, key=lambda task: (get_priority(task), -get_duration(task)))
+    return sorted(tasks, key=lambda task: (get_priority(task),
+                                           -parse_attribute(task, attributes['duration'].parse_regex)))
+
+
+def filter_tasks(tasks, api):
+    def have_elements_in_common(list1, list2):
+        return len(set(list1)) + len(set(list2)) != len(set(list1 + list2))
+
+    # TODO: Specific to my needs, a better solution would be to create a new label @no-planner and apply it to skipped
+    # tasks during labelling
+    labels = get_labels(api)
+    excluded_label_names = ['onhold', 'medecin', 'orsay', 'albert']
+    excluded_label_ids = [labels[label_name] for label_name in excluded_label_names]
+    return [task for task in tasks if not have_elements_in_common(task['labels'], excluded_label_ids)]
 
 
 if __name__ == '__main__':
@@ -148,13 +179,14 @@ if __name__ == '__main__':
     project_name = input('What project would you like to work on? ')
     project_id = get_project_id_by_name(project_name, api)
     tasks = get_active_tasks(project_id, api)
-    label_tasks(tasks)
+    tasks = filter_tasks(tasks, api)
+    label_tasks(tasks, api)
     sorted_tasks = sort_tasks(tasks)
     time_remaining = int(input('How long do you have? (minutes): '))
     selected_tasks = []
     for task in sorted_tasks:
         # TODO: Ask to split tasks that are too long
-        duration = get_duration(task)
+        duration = parse_attribute(task, attributes['duration'].parse_regex)
         if duration <= time_remaining:
             print(f'Selected: "{task["content"]}" ({duration}m)')
             selected_tasks.append(task)
